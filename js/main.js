be8cd2d1cdac05a0d002901d33f15c87e1bfe6fb
jashanbansal0708 <jashanbansal7856@yahoo.com>
2019-04-08 15:39:09 +0530

    Add the animations to the education section

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ window.addEventListener('scroll', function() {
   });
   
 
+
 window.addEventListener('scroll', function() {
 if (window.scrollY > 300) {
         var element = document.querySelector('#about');
@@ -18,3 +19,29 @@ if (window.scrollY > 300) {
         element.classList.add("dark");
     }
 });
+
+
+const items = document.querySelectorAll('.resume li');
+
+
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
