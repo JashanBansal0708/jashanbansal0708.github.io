28b6ab9c84d16087f045d9c9f175320e6c30a028
jashanbansal0708 <jashanbansal7856@yahoo.com>
2019-06-26 01:41:05 +0530

    Removed the other bugs and removed the animation bug this time very cleverly

--- js/main.js
+++ js/main.js
@@ -20,41 +20,43 @@ if (window.scrollY > 300) {
     }
 });
 
-
-const items = document.querySelectorAll('.resume li');
-const items2 = document.querySelectorAll('.skills li');
-const items3 = document.querySelectorAll('.certificates .certificate');
-
-const isInViewport = el => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-const run = () =>{
-  items.forEach(item => {
-    if (isInViewport(item)) {
-      item.classList.add('show');
-    }
-  });
-  items2.forEach(item => {
-    if (isInViewport(item)) {
-      item.classList.add('show');
-    }
-  });
-  items3.forEach(item => {
-    if (isInViewport(item)) {
-      item.classList.add('show');
-    }
-  });
-
-}
-
-window.addEventListener('load', run);
-window.addEventListener('resize', run);
-window.addEventListener('scroll', run);
+setTimeout(() => {
+  const items = document.querySelectorAll('.resume li');
+  const items2 = document.querySelectorAll('.skills li');
+  const items3 = document.querySelectorAll('.certificates .certificate');
+  
+  const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+  
+  const run = () =>{
+    items.forEach(item => {
+      if (isInViewport(item)) {
+        item.classList.add('show');
+      }
+    });
+    items2.forEach(item => {
+      if (isInViewport(item)) {
+        item.classList.add('show');
+      }
+    });
+    items3.forEach(item => {
+      if (isInViewport(item)) {
+        item.classList.add('show');
+      }
+    });
+  
+  }
+  
+  window.addEventListener('load', run);
+  window.addEventListener('resize', run);
+  window.addEventListener('scroll', run);
+    
+}, 2100);
